475df6a374502a2ffeed753d1e0357902d04dcca
generalpask <32198850+generalpask@users.noreply.github.com>
2019-02-09 18:22:48 +0100

    header+home+projects rework, social media links added in home

--- public/scripts/dynamic_switch.iced
+++
@@ -1,9 +0,0 @@
-document.getElementById('home').addEventListener 'click', ->
-  document.getElementById('iframe').src = 'home.html'
-  return
-document.getElementById('projects').addEventListener 'click', ->
-  document.getElementById('iframe').src = 'projects.html'
-  return
-document.getElementById('gallery').addEventListener 'click', ->
-  document.getElementById('iframe').src = 'gallery.html'
-  return
---
+++ public/scripts/gallery_switch.js
@@ -0,0 +1,11 @@
+(function() {
+    document.getElementById("0300h").addEventListener("click", function() {
+        document.getElementById("galleries-iframe").src="../public/galleries/0300h.html"
+    })
+    document.getElementById("samplegallery").addEventListener("click", function() {
+        document.getElementById("galleries-iframe").src="../public/galleries/samplegallery.html"
+    })
+    document.getElementById("skelly").addEventListener("click", function() {
+        document.getElementById("galleries-iframe").src="../public/galleries/skelly.html"
+    })
+}).call(this);
