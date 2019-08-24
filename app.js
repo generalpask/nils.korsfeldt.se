9e2b8ad5c2f024a1ff27b1225d427d29832ef80a
generalpask <32198850+generalpask@users.noreply.github.com>
2019-08-24 23:02:58 +0200

    Replaced cubes with a pink icosahedron

--- app.js
+++ app.js
@@ -9,35 +9,39 @@ app.use(express.static(__dirname + '/public/'));
 app.use('/', router);
 app.listen(process.env.port || 3000);
 
-router.get('/',function(req,res){
+app.get('/', function(req,res) {
     res.redirect('/index');
 });
 
-router.get('/index',function(req,res){
+app.get('/index', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
-router.get('/home',function(req,res){
+app.get('/sandbox', function(req, res) {
+    res.sendFile(path.join(__dirname+'/views/sandbox.html'));
+});
+
+app.get('/home', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/home.html'));
 });
-router.get('/projects',function(req,res){
+app.get('/projects', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/projects.html'));
 });
-router.get('/gallery',function(req,res){
+app.get('/gallery', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/gallery.html'));
 });
-router.get('/samplegallery',function(req,res){
+app.get('/samplegallery', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/galleries/samplegallery.html'));
 });
-router.get('/skelly',function(req,res){
+app.get('/skelly', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/galleries/skelly.html'));
 });
-router.get('/remembering',function(req,res){
+app.get('/remembering', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/galleries/remembering.html'));
 });
-router.get('/destination',function(req,res){
+app.get('/destination', function(req,res) {
     res.sendFile(path.join(__dirname+'/views/galleries/destination.html'));
 });
 
-console.log('nils.korsfeldt.se is now active')
+console.log('nils.korsfeldt.se is now active');
 console.log('Running locally at port 3000');
--- public/scripts/cubes.js
+++ public/scripts/cubes.js
@@ -1,25 +1,57 @@
+/*
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer1 = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer();
 
 renderer1.setSize(window.innerWidth, window.innerHeight);
 renderer1.setClearColor( 0xffffff, 1);
 document.body.appendChild(renderer1.domElement);
 
+var L1 = new THREE.PointLight( 0xffffff, 1);
+L1.position.z = 100;
+L1.position.y = 100;
+L1.position.x = 100;
+scene.add(L1);
 
-var boxGeometry111 = new THREE.BoxGeometry(1,1,1);
+var L2 = new THREE.PointLight( 0xffffff, 0.8);
+L2.position.z = 200;
+L2.position.y = 50;
+L2.position.x = -100;
+scene.add(L2);
+
+var box111 = new THREE.BoxGeometry(1,1,1);
+var icosahedron10 = new THREE.IcosahedronGeometry(1,0);
 var blackWireframe = new THREE.MeshBasicMaterial( {
     color: 0x000000,
     wireframe: true,
 });
+var black = new THREE.MeshBasicMaterial( {
+    color: 0x000000,
+});
+var white = new THREE.MeshBasicMaterial( {
+    color: 0xffffff,
+})
+var reflectiveBlack = new THREE.MeshPhongMaterial( {
+    color: 0x000000,
+})
 
-var cube = new THREE.Mesh(boxGeometry111, blackWireframe);
-var cube2 = new THREE.Mesh(boxGeometry111, blackWireframe);
+
+
+var cube = new THREE.Mesh(box111, reflectiveBlack);
+var cube2 = new THREE.Mesh(box111, reflectiveBlack);
+var icosahedron = new THREE.Mesh(icosahedron10, blackWireframe);
+
+var edges = new THREE.LineSegments(box111, white);
+cube.add(edges);
+
 
 scene.add(cube);
 scene.add(cube2);
+scene.add(icosahedron);
+scene.add(edges);
 cube.position.set(-4,0,0);
 cube2.position.set(4,0,0);
+icosahedron.position.set(-4,2,0);
 
 camera.position.z = 5;
 
@@ -28,9 +60,11 @@ var update = function() {
     cube.rotation.y += 0.005;
     cube2.rotation.x += 0.005;
     cube2.rotation.y += 0.005;
+    icosahedron.rotation.x += 0.005;
+    icosahedron.rotation.y += 0.005;
 }
 var render = function() {
-    renderer1.render(scene, camera);
+    renderer.render(scene, camera);
 }
 var GameLoop = function() {
     requestAnimationFrame(GameLoop);
@@ -39,3 +73,56 @@ var GameLoop = function() {
 }
 
 GameLoop();
+*/
+var $container = $('.main');
+var renderer = new THREE.WebGLRenderer({antialias: true});
+var camera = new THREE.PerspectiveCamera(80,1,0.1,10000);
+var scene = new THREE.Scene();
+
+scene.add(camera);
+renderer.setSize(window.innerHeight, window.innerHeight);
+renderer.setClearColor( 0xffffff, 1);
+$container.append(renderer.domElement);
+
+///////////////////////////////////////////////
+
+// Camera
+camera.position.z = 200;
+
+// Material
+var pinkMat = new THREE.MeshPhongMaterial({
+  color      :  new THREE.Color("rgb(226,35,213)"),
+  emissive   :  new THREE.Color("rgb(255,128,64)"),
+  specular   :  new THREE.Color("rgb(255,155,255)"),
+  shininess  :  10,
+  shading    :  THREE.FlatShading,
+  transparent: 1,
+  opacity    : 1
+});
+
+var L1 = new THREE.PointLight( 0xffffff, 1);
+L1.position.set(100, 100, 100);
+scene.add(L1);
+
+var L2 = new THREE.PointLight( 0xffffff, 0.8);
+L2.position.set(-100, 50, 200);
+scene.add(L2);
+
+
+var Ico = new THREE.Mesh(new THREE.IcosahedronGeometry(60,0), pinkMat);
+Ico.rotation.z = 0.5;
+scene.add(Ico);
+
+function update(){
+   Ico.rotation.x+=2/100;
+   Ico.rotation.y+=2/100;
+}
+
+// Render
+function render() {
+  requestAnimationFrame(render);			
+  renderer.render(scene, camera);	
+  update();
+}
+
+render();
